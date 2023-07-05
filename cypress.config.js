58dd8e12d79dd60496edca58d93f64c172b1706e
SyedZeeshanAli12 <syedzeeshanali877@gmail.com>
2023-07-05 12:52:57 +0500

    adding to config

--- cypress.config.js
+++ cypress.config.js
@@ -10,5 +10,19 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    "reporter": "cypress-multi-reporters",
+    "reporterOptions": {
+      "reporterEnabled": "mocha-junit-reporter, mochawesome",
+      "mochaJunitReporterReporterOptions": {
+        "mochaFile": "cypress/reports/junit/test-results.[hash].xml",
+        "toConsole": true
+      },
+      "mochawesomeReporterOptions": {
+        "reportDir": "cypress/reports/mochawesome",
+        "overwrite": false,
+        "html": false,
+        "json": true
+      }
+    }
   },
 });
